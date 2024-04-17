56d41c172db59112e89ed7cbee53499e6abb8998
James <zijianliu.joy@gmail.com>
2024-04-17 01:13:38 -0700

    remove images.unoptimized in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,15 +3,15 @@ const nextConfig = {
     basePath: "/homepage",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
+    // /**
+    //  * Disable server-based image optimization. Next.js does not support
+    //  * dynamic features with static exports.
+    //  *
+    //  * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+    //  */
+    // images: {
+    //   unoptimized: true,
+    // },
 };
 
 export default nextConfig;
